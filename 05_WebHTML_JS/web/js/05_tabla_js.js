20ea08c11c16fa1404f2d1eaed4b581229fd911f
Ricardo Pliego <rpliegomere@gmail.com>
2019-05-17 17:49:45 +0200

    Clase 9

--- 05_WebHTML_JS/web/js/05_tabla_js.js
+++ 05_WebHTML_JS/web/js/05_tabla_js.js
@@ -8,22 +8,66 @@
 console.log("Generar tabla...");
 generarTabla();
 function generarTabla(){
+    var nomTipo = document.getElementById("tipo3").value;
+    var nomNombre = document.getElementById("nombre2").value;
+    var nomPrecio = document.getElementById("precio").value;
+    var nomStock = document.getElementById("stock").value;
+    
     var table = document.getElementsByTagName("table");
-    var nombrePrecio = "Precio cosasasasa";
+    var nombrePrecio = "Precio cosas";
     table = table[0];
     table.innerHTML = "<caption>Lista de vainas</caption>";
     table.innerHTML += `<thead>
     <tr><th>Tipo</th>
         <th>Nombre</th>
-        <th>${nombrePrecio}</th></tr>
+        <th>${nombrePrecio}</th>
+        <th>Stock</th></tr>
 </thead>`;
     
     var lista = new Array();
-    lista  = [["algo1", "algo2", "algo3"]];
-    lista [1] = ["algo4", "algo5", "algo6"];
-    lista [2] = ["algo7", "algo8", "algo9"];
+    lista [0]= ["algo1", "algo2", "algo3", "7"];
+    lista [1] = ["algo4", "algo5", "algo6", "2"];
+    lista [2] = ["algo7", "algo8", "algo9", "6"];
+    lista [3] = [nomTipo, nomNombre, nomPrecio, nomStock];
     
     for (var i = 0; i < lista.length; i++){
+        var nodoTR = document.createElement("tr");
+        table.appendChild(nodoTR);
+        for (var columna of lista[i]){
+            var nodoCol = document.createElement("td");
+            nodoTR.appendChild(nodoCol);
+            var textoCol = document.createTextNode(columna);
+            nodoCol.appendChild(textoCol);
+            
+        }
+    }
+}
+
+
+function generarTablaV2(){
+    var nomTipo = document.getElementById("tipo3").value;
+    var nomNombre = document.getElementById("nombre2").value;
+    var nomPrecio = document.getElementById("precio").value;
+    var nomStock = document.getElementById("stock").value;
+    
+    var table = document.getElementsByTagName("table");
+    var nombrePrecio = "Precio cosas";
+    table = table[0];
+    table.innerHTML = "<caption>Lista de vainas</caption>";
+    table.innerHTML += `<thead>
+    <tr><th>Tipo</th>
+        <th>Nombre</th>
+        <th>${nombrePrecio}</th>
+        <th>Stock</th></tr>
+</thead>`;
+    
+    var lista = new Array();
+    lista [lista.length] = [nomTipo, nomNombre, nomPrecio, nomStock];
+   for (var i = 0; i < lista.length; i++){
+   
+    lista [i] = [nomTipo, nomNombre, nomPrecio, nomStock];
+    
+     
         var nodoTR = document.createElement("tr");
         table.appendChild(nodoTR);
         for (var columna of lista[i]){
