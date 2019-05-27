9e55e405a54ffd0775e95089de3810f60a92fc20
Ricardo Pliego <rpliegomere@gmail.com>
2019-05-27 14:00:54 +0200

    Clase 15.1

--- AplicacionWebVentas/WebVentas/web/js/cookies.js
+++ AplicacionWebVentas/WebVentas/web/js/cookies.js
@@ -1,20 +1,20 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-document.getElementById("cookie_nom").innerHTML = getCookie("nombre_busqueda"); //document.cookie;
+document.getElementById("span_cookie_nom").innerHTML 
+        = getCookie("nombre_busqueda"); // document.cookie
+// otra_cook=asdfasdfads
+//      nombre_busqueda=jaklskdf sdf
+//  otra_c=jaklskdf sdf
 
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
+  //"   aafdsasdf "
   for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    var c = ca[i].trim();
+   /* while (c.charAt(0) == ' ') {
       c = c.substring(1);
-    }
+    }*/
     if (c.indexOf(name) == 0) {
       return c.substring(name.length, c.length);
     }
